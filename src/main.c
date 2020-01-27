829a17e021afceb9481d59a6c89261a367c4e79d
Keenan Turley <keenan.turley@gmail.com>
2020-01-27 04:21:49 -0500

    Implement basic main program structure

--- src/main.c
+++ src/main.c
@@ -1,8 +1,26 @@
+/*
+Author: Keenan Turley <keenan.turley@knights.ucf.edu>
+Date: 2020-01-27
+Course: COP3402 - Systems Software
+Assignment: Homework #1 (P-Machine)
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 #include "pm0.h"
+#include "loader.h"
 
-int main(void) {
-    printf("PM/0 VM\n");
+int main(int argc, char ** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: ./pm0vm <file-path>\n");
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    p_machine * vm = create_machine();
+    load(argv[1], vm);
+
+    run(vm);
+
+    vm = destroy(vm);
+    return EXIT_SUCCESS;
 }
