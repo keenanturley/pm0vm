479c4be3980a1626fc10401ae675a64f21483f88
Keenan Turley <keenan.turley@gmail.com>
2020-01-26 23:00:09 -0500

    Implement source code loader

---
+++ src/loader.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "loader.h"
+
+void parse_instruction(char * line, instruction * i) {
+    char * op = strtok(line, " ");
+    char * r  = strtok(NULL, " ");
+    char * l  = strtok(NULL, " ");
+    char * m  = strtok(NULL, " ");
+
+    if (op == NULL || r == NULL || l == NULL || m == NULL) {
+        fprintf(stderr, "Invalid instruction format, exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    i->op = atoi(op);
+    i->r  = atoi(r);
+    i->l  = atoi(l);
+    i->m  = atoi(m);
+}
+
+void load(char * file_path, p_machine * vm) {
+    FILE * fp = fopen(file_path, "r");
+    char * line = NULL;
+    size_t n = 0;
+    int line_num = 0;
+
+    while (getline(&line, &n, fp) != -1) {
+        parse_instruction(line, &(vm->code[line_num]));
+        line_num++;
+    }
+
+    if (line) {
+        free(line);
+    }
+
+    fclose(fp);
+}
---
+++ src/loader.h
@@ -0,0 +1,26 @@
+#ifndef LOADER_H
+#define LOADER_H
+
+#include "pm0.h"
+
+/*
+Function: parse_instruction
+    Parses and places the instruction segments from a given string into the
+    instruction structure pointed to by i.
+Parameters:
+    char * line: The line to read an instruction from
+    instruction * i: A pointer to the instruction to write values to
+Note:
+    The line should be composed as a sequence of 4 integers, compliant with
+    the PM/0 specification, each separated by a single space character.
+*/
+void parse_instruction(char * line, instruction * i);
+
+/*
+Function: load
+    Attempts to load the instructions included in the given file into the
+    code memory of the given vm.
+*/
+void load(char * file_path, p_machine * vm);
+
+#endif /* LOADER_H */
