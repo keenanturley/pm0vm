ef55ff81eadf31d3cfedc069f6e8c97b395abd4c
Keenan Turley <keenan.turley@gmail.com>
2020-01-29 18:45:34 -0500

    Complete comment of load function

--- src/loader.h
+++ src/loader.h
@@ -20,6 +20,11 @@ void parse_instruction(char * line, instruction * i);
 Function: load
     Attempts to load the instructions included in the given file into the
     code memory of the given vm.
+Parameters:
+    char * file_path:
+        The path to a file containing assembly instructions in integer form
+    p_machine * vm:
+        The machine to load instructions into
 */
 void load(char * file_path, p_machine * vm);
 
